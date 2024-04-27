b29a33bf44259a0585fd769a1e31963808e6704e
danikxd <danmarek111@gmail.com>
2024-04-27 18:14:13 +0200

    Přidejte soubory projektu.

---
+++ boruvkuv_algo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace boruvkuv_algo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
