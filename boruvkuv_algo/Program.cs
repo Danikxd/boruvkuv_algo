cbe637f06ef66daf3db37fb2b3dad263ae1f98cc
danikxd <danmarek111@gmail.com>
2024-05-02 18:41:59 +0200

    last commit

--- boruvkuv_algo/Program.cs
+++ boruvkuv_algo/Program.cs
@@ -4,12 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Graph g = new Graph(4);
-            g.AddEdge(0, 1, 10);
+            /*  Graph g = new Graph(4);
+              g.AddEdge(0, 1, 10);
+              g.AddEdge(0, 2, 6);
+              g.AddEdge(0, 3, 5);
+              g.AddEdge(1, 3, 15);
+              g.AddEdge(2, 3, 4); */
+
+            Graph g = new Graph(12);
+            g.AddEdge(0, 1, 13);
             g.AddEdge(0, 2, 6);
-            g.AddEdge(0, 3, 5);
-            g.AddEdge(1, 3, 15);
-            g.AddEdge(2, 3, 4);
+            g.AddEdge(1, 2, 7);
+            g.AddEdge(1, 3, 1);
+            g.AddEdge(2, 3, 14);
+            g.AddEdge(2, 4, 8);
+            g.AddEdge(2, 7, 20);
+            g.AddEdge(3, 4, 9);
+            g.AddEdge(3, 5, 3);
+            g.AddEdge(4, 5, 2);
+            g.AddEdge(4, 9, 18);
+            g.AddEdge(6, 7, 15);
+            g.AddEdge(6, 8, 5);
+            g.AddEdge(6, 9, 19);
+            g.AddEdge(6, 10, 10);
+            g.AddEdge(7, 9, 17);
+            g.AddEdge(8, 10, 11);
+            g.AddEdge(9,10, 16);
+            g.AddEdge(9, 11, 4);
+            g.AddEdge(10, 11, 12);
+
+
+
+
+
+
+
+
+
+
 
             g.BoruvkaAlgorithm();
         }
@@ -42,7 +74,7 @@
 
                 int numTrees = V; //nazačátku máme V stromů
                 int TotalPrice = 0; //celková cena
-
+                
 
                 //založ V počet cest s 1 elementem
                 for (int node = 0; node < V; node++)
@@ -55,17 +87,17 @@
                 while (numTrees > 1) //dokud nejsou všechny stromy spojené pokračuj
                 {
 
-                    for (int i = 0; i < graph.Count; i++)
+                    for (int i = 0; i < graph.Count; i++)  //Pro každou hranu se zjišťuje, zda spojuje dvě různé komponenty grafu. Pokud ano, porovnává se cena této hrany s cenou uloženou v seznamu cheapest pro příslušné komponenty
                     {
                         int u = graph[i][0];
                         int v = graph[i][1];
                         int w = graph[i][2]; //načítání hodnot z grafu
 
                         int set1 = Find(parent, u);
-                        int set2 = Find(parent, v); //najdi dva komponenty aktualni hrani
+                        int set2 = Find(parent, v); //najdi do jaké množiny prvky patří
 
 
-                        if (set1 != set2)
+                        if (set1 != set2) //pokud se nerovnají assetni nejmenší cenu
                         {
                             if (cheapest[set1][2] == -1
                                 || cheapest[set1][2] > w)
@@ -100,11 +132,11 @@
                                 v = cheapest[node][1],
                                 w = cheapest[node][2];
                             int set1 = Find(parent, u),
-                                set2 = Find(parent, v);
+                                set2 = Find(parent, v);  //najde do jake množiny dva prvky patří
                             if (set1 != set2)
                             {
                                 TotalPrice += w;
-                                UnionSet(parent, rank, set1, set2);
+                                UnionSet(parent, rank, set1, set2); //pokud není stejná spoj je
                                 Console.WriteLine(
                                     "Edge {0}-{1} with weight {2} included in MST",
                                     u, v, w);
@@ -122,7 +154,7 @@
                 }
 
                 Console.WriteLine("Weight of MST is {0}",
-                          TotalPrice);
+                          TotalPrice); //vypiš celkovou cenu
 
 
 
@@ -134,7 +166,7 @@
                          int x, int y)
             {
                 int xroot = Find(parent, x);
-                int yroot = Find(parent, y);
+                int yroot = Find(parent, y); //najdí kořeny obou množin
 
               
                 if (rank[xroot] < rank[yroot]) //menší strom zapiš pod větší 
